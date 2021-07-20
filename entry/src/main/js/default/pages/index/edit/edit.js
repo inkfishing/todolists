53f8c7b5131aef8dbe1ac0fcbfb714f41f179b18
意识流 <ink_fishing@163.com>
2021-07-20 15:51:55 +0800

    UI界面更新

--- entry/src/main/js/default/pages/index/edit/edit.js
+++ entry/src/main/js/default/pages/index/edit/edit.js
@@ -1,5 +1,95 @@
+import router from '@system.router';
+const SAVE_RECORD = 101;
+const ACTION_SYNC = 0;
+const ABILITY_TYPE_INTERNAL = 1;
+
 export default {
     data: {
-        title: 'World'
+        isRestore: false,
+        titleStr: 'default title',
+        titleContent: 'default content',
+        tag: "editpage",
+        continueAbilityData: {
+            titleStr: 'default title',
+            titleContent: 'default content',
+        }
+    },
+    onInit() {
+        console.info("onInit " + this.tag);
+    },
+    onReady() {
+        console.info("onReady " + this.tag);
+    },
+    onShow() {
+        console.info("onShow " + this.tag);
+    },
+    onHide() {
+        console.info("onHide " + this.tag);
+    },
+    onChange(e) {
+        if (!this.isRestore) {
+            this.titleContent = e.text;
+        }
+        this.isRestore = false;
+    },
+    async migrate() {
+        console.info("click migrate ");
+        //
+        var result = await FeatureAbility.continueAbility();
+        console.info("result:" + JSON.stringify(result));
+    },
+    async saveRecords(keyString, valueString) {
+        console.info("save Note " + keyString + valueString);
+        var record = {
+            "key": keyString,
+            "value": valueString
+        };
+        var actionData = JSON.stringify(record);
+        var action = {};
+        action.bundleName = 'com.ohos.demo.uitest';
+        action.abilityName = 'com.ohos.demo.uitest.ComputeInternalAbility';
+        action.messageCode = SAVE_RECORD;
+        action.data = actionData;
+        action.abilityType = ABILITY_TYPE_INTERNAL;
+        action.syncOption = ACTION_SYNC;
+        var result = await FeatureAbility.callAbility(action);
+        var ret = JSON.parse(result);
+        if (ret.code == 0) {
+            console.info('saveRecords result is:' + JSON.stringify(ret.abilityResult));
+        } else {
+            console.info('saveRecords error code:' + JSON.stringify(ret.code));
+        }
+        router.push({
+            uri: 'pages/index/index',
+            params: {
+            },
+        });
+    },
+    onStartContinuation: function () {
+        // 判断当前的状态是不是适合迁移
+        console.info("onStartContinuation");
+        return true;
+    },
+    onCompleteContinuation: function (code) {
+        // 迁移操作完成，code返回结果
+        console.info("nCompleteContinuation: code = " + code);
+    },
+    onSaveData: function (saveData) {
+        // 数据保存到savedData中进行迁移。
+        this.continueAbilityData.titleStr = this.titleStr;
+        this.continueAbilityData.titleContent = this.titleContent;
+        console.info("onSaveData: string = " + this.titleStr + "," + this.titleContent);
+        var data = this.continueAbilityData;
+        Object.assign(saveData, data)
+        console.info("onSaveData: data = " + data.titleStr + data.titleContent);
+    },
+    onRestoreData: function (restoreData) {
+        // 收到迁移数据，恢复。
+        this.isRestore = true;
+        this.continueAbilityData = restoreData;
+        this.titleStr = this.continueAbilityData.titleStr;
+        this.titleContent = this.continueAbilityData.titleContent;
+        this.$refs.content.value = this.titleContent;
+        console.info("onRestoreData: data = " + this.titleStr + ", " + this.titleContent);
     }
 }
--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,8 +1,78 @@
+import router from '@system.router';
+
+const GET_RECORD = 100;
+const SAVE_RECORD = 101;
+const ACTION_SYNC = 0;
+const ACTION_ASYNC = 1;
+const ABILITY_TYPE_EXTERNAL = 0;
+const ABILITY_TYPE_INTERNAL = 1;
+
 export default {
     data: {
-        title: ""
+        todolists: [
+        ],
+        tag: "mainpage"
     },
     onInit() {
         this.title = this.$t('strings.world');
+    },
+    onReady() {
+        console.info("onReady " + this.tag);
+    },
+    onShow() {
+        console.info("onShow " + this.tag);
+        this.getList();
+    },
+    onHide() {
+        console.info("onHide " + this.tag);
+    },
+    newNote() {
+        console.info("click new Note");
+        router.push({
+            uri: 'pages/editpage/editpage',
+            params: {
+                titleStr: 'title'+this.todolist.length,
+                titleContent: '',
+            },
+        });
+    },
+    async getList() {
+        console.info("getList ");
+        var action = {};
+        action.bundleName = 'com.ohos.demo.uitest';
+        action.abilityName = 'com.ohos.demo.uitest.ComputeInternalAbility';
+        action.messageCode = GET_RECORD;
+        action.data = {};
+        action.abilityType = ABILITY_TYPE_INTERNAL;
+        action.syncOption = ACTION_SYNC;
+
+
+        var result = await FeatureAbility.callAbility(action);
+        var ret = JSON.parse(result);
+        console.info("getList result ");
+        if (ret.code == 0) {
+            console.info('getList before result is:' + JSON.stringify(ret.recordList) + ",todolist="+this.todolist);
+            var list = [];
+            for (var index = 0; index < ret.recordList.length; index++) {
+                const element = {};
+                element.title = ret.recordList[index].mTitle;
+                element.content = ret.recordList[index].mString;
+                list.push(element);
+            }
+            this.todolist = list;
+            console.info('getList result is:' + JSON.stringify(ret.recordList) + ",todolist="+this.todolist+",size="+this.todolist.length);
+        } else {
+            console.info('getList error code:' + JSON.stringify(ret.code));
+        }
+    },
+    editItem(title, content) {
+        console.info("editItem Note " + title + ":" + content);
+        router.push({
+            uri: 'pages/editpage/editpage',
+            params: {
+                titleStr: title,
+                titleContent: content,
+            },
+        });
     }
 }
